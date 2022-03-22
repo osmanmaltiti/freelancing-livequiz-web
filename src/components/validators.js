6acc27ad5608c07769b0b6af8c00100e87df3f1f
osmanmaltiti <terabyte7778@gmail.com>
2022-03-22 06:06:59 -1200

    validators update

--- src/components/validators.js
+++ src/components/validators.js
@@ -3,9 +3,7 @@ const Validate = () => {
     
     const validateLogin = (values) => {
         const errors = {};
-        if (!values.email) {
-            errors.email = <IoAlertCircleOutline/>;
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
             errors.email = 'Invalid email address';
         }
         if (!values.password) {
@@ -19,17 +17,11 @@ const Validate = () => {
         const errors = {};
         if (!values.Firstname) {
             errors.Firstname = <IoAlertCircleOutline/>;
-        } else if (values.Firstname.length > 15) {
-            errors.Firstname = 'Must be 25 characters or less';
         }
         if (!values.Lastname) {
             errors.Lastname = <IoAlertCircleOutline/>;
-        } else if (values.Lastname.length > 15) {
-            errors.Lastname = 'Must be 25 characters or less';
         }
-        if (!values.Email) {
-            errors.Email = <IoAlertCircleOutline/>;
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.Email)) {
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.Email)) {
             errors.Email = 'Invalid email address';
         }
         if (!values.Password) {
